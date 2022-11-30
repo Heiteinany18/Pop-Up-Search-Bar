8938f28a553dd1e7f9479c0ce1d16685d539c730
David Mphande <102615835+Heiteinany18@users.noreply.github.com>
2022-11-30 16:44:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+/*  For Open Button  */
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+ 
+  /* document.getElementById("coder-details").style.display = "none";*/
+}
+
+/*  For Close Button  */
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+  /* document.getElementById("coder-details").style.display = "block";*/
+}
